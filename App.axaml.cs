c0e65c2ad5fe2a3c3f9191e07445705d36b65d67
AshFungor <ash_fungor@aol.com>
2022-02-01 23:34:55 +0300

    main login controller functionality

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,14 +1,34 @@
+#define DEBUG
+#define LINUX
+
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using System;
+using Avalonia.Logging;
+using PowerMonitor.controllers;
+
+
+
 
 namespace PowerMonitor
 {
     public class App : Application
     {
+        #if LINUX && !DEBUG
+        public static string SettingsPath => $"/home/{Environment.UserName}/.config/PowerMonitor";
+        #elif LINUX && DEBUG
+        // change path to your local resources
+        public static string SettingsPath => $"/home/{Environment.UserName}/Documents/";
+        #endif
+        public LoginController LoginController;
+
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            LoginController = new LoginController();
         }
 
         public override void OnFrameworkInitializationCompleted()
--- controllers/LoginController.cs
+++ controllers/LoginController.cs
@@ -1,6 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
 namespace PowerMonitor.controllers;
 
-public class LoginController
+public sealed class LoginController
 {
-    
+    private static string logins_file = ".logins.xml";
+    public UserInfoCollection Users { get; set; }
+
+    public sealed class UserInfo
+    {
+        public string Name { get; set; }
+        [XmlArrayAttribute]
+        public List<string> Restrictions { get; set; }
+        public string Password { get; set; }
+
+        public UserInfo(string name, string password, List<string> rests)
+        {
+            Name = name;
+            Password = password;
+            Restrictions = rests;
+        }
+        public  UserInfo() {}
+    }
+
+    public sealed class UserInfoCollection
+    {
+        [XmlArrayAttribute]
+        public UserInfo[] UserInfoList { get; set; }
+    }
+
+    public LoginController()
+    {
+        if (File.Exists(App.SettingsPath + logins_file))
+        {
+            StreamReader istream = new StreamReader(App.SettingsPath + logins_file);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(UserInfoCollection));
+            UserInfoCollection user_coll = null;
+            try
+            {
+                user_coll = xmlSerializer.Deserialize(istream) as UserInfoCollection;
+            }
+            catch (XmlException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            Users = user_coll;
+        }
+        else
+        {
+            StreamWriter ostream = new StreamWriter(App.SettingsPath + logins_file);
+            UserInfo admin = new UserInfo("admin", "password", new List<string>());
+            XmlSerializer xml_serializer = new XmlSerializer(typeof(UserInfoCollection));
+            UserInfoCollection user_coll = new UserInfoCollection() {UserInfoList = new[] {admin}};
+            xml_serializer.Serialize(ostream, user_coll);
+            Users = user_coll;
+        }
+
+
+    }
+
+    public void UpdateLogins()
+    {
+        File.Delete(App.SettingsPath + logins_file);
+        StreamWriter ostream = new StreamWriter(App.SettingsPath + logins_file);
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(UserInfoCollection));
+        xmlSerializer.Serialize(ostream, Users);
+    }
+
+
 }
