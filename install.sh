afb07fafb7cd9e20b7801bc4a5079b627417b365
AshFungor <ash_fungor@aol.com>
2022-02-25 19:32:09 +0300

    updating script

--- install.sh
+++ install.sh
@@ -2,3 +2,13 @@
 
 # install script for linux just in case
 
+printf "####################\n" 
+printf "#    welcome to    #\n"
+printf "#   PowerMonitor   #\n"
+printf "#    installer     #\n"
+printf "####################\n\n"
+
+
+printf "this script compiles and installs PowerMonitor from source code\n"
+printf "for source see: https://github.com/AshFungor/PowerMonitor\n"
+
