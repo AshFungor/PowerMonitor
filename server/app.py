05217911f54b5727078c01e045554aa4a1a08977
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-08 17:45:59 +0300

    added create_user function

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, send_file
-from utils.database_api.database import Database
+from server.utils.database_api.database import Database
+from server.data.config import NAME, USER, PASSWORD, HOST
 
 
 app = Flask(__name__)
-database = Database()
+database = Database(NAME, USER, PASSWORD, HOST)
 
 
 @app.route('/<int:serial_number>/<string:file_name>', methods=['GET'])
@@ -11,11 +12,13 @@ def send_data(serial_number: int, file_name: str):
     return send_file(f'PredprofExamples/{file_name}.csv')
 
 
-@app.route('/<int:id>/<string:name>')
-def create_user(id: int, name: str):
-    database.add_user(id, name)
+@app.route('/create-user/<string:login>/<string:password>/<int:is_admin>')
+def create_user(login: str, password: str, is_admin: int):
+    is_admin = bool(is_admin)
+    database.add_user(login, password, is_admin)
     return
 
 
 if __name__ == '__main__':
+    database.initiate()
     app.run()
