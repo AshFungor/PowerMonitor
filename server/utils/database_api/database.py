3f634844adf8a6cd41e2fb6143e863f3abf6bbdc
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-12 00:01:53 +0300

    updated telemetry functions

--- server/utils/database_api/database.py
+++ server/utils/database_api/database.py
@@ -1,4 +1,3 @@
-from werkzeug.security import generate_password_hash
 import psycopg2
 
 
@@ -18,7 +17,7 @@ class Database:
             host=self.host
         )
 
-    def execute(self, sql, parameters, fetchone, fetchall, commit):
+    def execute(self, sql, parameters=None, fetchone=False, fetchall=False, commit=False):
         parameters = tuple() if parameters is None else parameters
         connection = self.connection
         with connection.cursor() as cursor:
@@ -33,7 +32,7 @@ class Database:
 
             return data
 
-    def create_table_users(self):
+    def _create_table_users(self):
         sql = """
         CREATE TABLE IF NOT EXISTS
             users (
@@ -46,54 +45,79 @@ class Database:
         """
         self.execute(sql, commit=True)
 
-    def create_table_complexes(self):
+    def _create_table_complexes(self):
         sql = """
         CREATE TABLE IF NOT EXISTS
             complexes (
                 id SERIAL PRIMARY KEY,
                 serial_number INTEGER NOT NULL,
-                user_id INTEGER NOT NULL
+                user_login VARCHAR(255) NOT NULL
             )
         ;
         """
         self.execute(sql, commit=True)
 
-    def create_table_telemetry(self):
+    def _create_table_telemetry(self):
         sql = """
         CREATE TABLE IF NOT EXISTS
-            telemetry (
+            telemetry(
                 id SERIAL PRIMARY KEY,
-                serial_number INTEGER NOT NULL,
-                path_to_file VARCHAR(255) NOT NULL,
-                period VARCHAR(255) NOT NULL
+                serial_number INTEGER,
+                start timestamp without time zone NOT NULL,
+                finish timestamp without time zone NOT NULL,
+                reactive_power_by_phase_A FLOAT,
+                reactive_power_by_phase_B FLOAT,
+                reactive_power_by_phase_C FLOAT,
+                active_power_by_phase_A FLOAT,
+                active_power_by_phase_B FLOAT,
+                active_power_by_phase_C FLOAT,
+                voltage_by_phase_A FLOAT,
+                voltage_by_phase_B FLOAT,
+                voltage_by_phase_C FLOAT,
+                cos_by_phase_A FLOAT,
+                cos_by_phase_B FLOAT,
+                cos_by_phase_C FLOAT,
+                reactive_power_by_phase_A_off FLOAT,
+                reactive_power_by_phase_B_off FLOAT,
+                reactive_power_by_phase_C_off FLOAT,
+                active_power_by_phase_A_off FLOAT,
+                active_power_by_phase_B_off FLOAT,
+                active_power_by_phase_C_off FLOAT,
+                voltage_by_phase_A_off FLOAT,
+                voltage_by_phase_B_off FLOAT,
+                voltage_by_phase_C_off FLOAT,
+                cos_by_phase_A_off FLOAT,
+                cos_by_phase_B_off FLOAT,
+                cos_by_phase_C_off FLOAT,
+                number_of_enabled_blocks INTEGER
             )
         ;
         """
         self.execute(sql, commit=True)
 
     def initiate(self):
-        self.create_table_users()
-        self.create_table_complexes()
-        self.create_table_telemetry()
+        self._create_table_users()
+        self._create_table_complexes()
+        self._create_table_telemetry()
 
-    def add_user(self, login, password, is_admin):
-        password = generate_password_hash(password)
+    def add_user(self, user):
         sql = """
         INSERT INTO users(login, password, is_admin) VALUES(%s, %s, %s)
         """
-        self.execute(sql, parameters=(login, password, is_admin), commit=True)
+        self.execute(sql, parameters=(user.login, user.password, user.is_admin), commit=True)
 
-    def add_telemetry(self, serial_number, path_to_file, period):
-        sql = """
-                INSERT INTO telemetry(serial_number, path_to_file, period) VALUES(%s, %s, %s)
-                """
-        self.execute(sql, parameters=(serial_number, path_to_file, period), commit=True)
+    def add_telemetry(self, telemetry):
+        sql = f"""
+        INSERT INTO telemetry({', '.join(telemetry.parameter_names)})
+        VALUES({', '.join(["%s"] * len(telemetry.parameter_names))})
+        """
+        self.execute(sql, parameters=telemetry.parameters, commit=True)
 
-    def add_complex(self, serial_number, user_id):
+    def add_complex(self, complex):
         sql = """
-                INSERT INTO complexes(serial_number, users) VALUES(%s, %s)
-                """
-        self.execute(sql, parameters=(serial_number, user_id), commit=True)
+        INSERT INTO complexes(serial_number, user_login) VALUES(%s, %s)
+        """
+        self.execute(sql, parameters=(complex.serial_number, complex.user_login), commit=True)
 
     def select_user_by_login(self, login):
         sql = """
