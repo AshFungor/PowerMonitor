1f1b9f95a096a6a620e88049e9fa8adc9eb35640
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-11 23:41:25 +0300

    http requests are working now

--- server/app.py
+++ server/app.py
@@ -1,22 +1,49 @@
-from flask import Flask, send_file
+from flask import Flask, make_response, send_file, request
+
 from server.utils.database_api.database import Database
 from server.data.config import NAME, USER, PASSWORD, HOST
-
+from server.utils.csv_api.csv_parser import parse_csv
+from server.utils.request_parser import parse_user_info
+from server.utils.database_api.tables import User, Telemetry, Complex
 
 app = Flask(__name__)
 database = Database(NAME, USER, PASSWORD, HOST)
 
 
-@app.route('/<int:serial_number>/<string:file_name>', methods=['GET'])
-def send_data(serial_number: int, file_name: str):
-    return send_file(f'PredprofExamples/{file_name}.csv')
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    json = request.json
+    request_ = parse_user_info(json)
+    user = User(*request_)
+    response = make_response(send_file(r'C:\Users\Zero\PycharmProjects\postgra\files\1607582294.csv'))
+    response.headers["Content-type"] = "text/csv"
+    response.status_code = 200
+    return response
 
 
-@app.route('/create-user/<string:login>/<string:password>/<int:is_admin>')
-def create_user(login: str, password: str, is_admin: int):
-    is_admin = bool(is_admin)
-    database.add_user(login, password, is_admin)
-    return
+@app.route('/create-user', methods=['POST'])
+def create_user():  # login: str, password: str, is_admin: bool
+    request_ = parse_user_info(request.json)
+    user = User(*request_['user'])
+    for serial_number in request_['complexes']:
+        complex = Complex(user.login, serial_number)
+        database.add_complex(complex)
+    database.add_user(user)
+    response = make_response()
+    response.status_code = 200
+    return response
+
+
+@app.route('/send-telemetry/<int:serial_number>', methods=['POST'])
+def send_telemetry(serial_number: int):
+    print(serial_number)
+    request_ = request.files['table']
+    csv_file = request_
+    table = parse_csv(csv_file)
+    table = [Telemetry(serial_number, *measurements) for measurements in table]
+    for telemetry in table:
+        database.add_telemetry(telemetry)
+    return '200'
 
 
 if __name__ == '__main__':
