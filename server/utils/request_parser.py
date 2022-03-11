0d648196ec692755eb1f26a4a932519ea6025645
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-11 23:56:45 +0300

    json thing

---
+++ server/utils/request_parser.py
@@ -0,0 +1,19 @@
+from typing import Dict, Tuple, Union, List, Optional, Any
+
+
+UserData = Dict[str, Union[str, bool, List[Optional[int]]]]
+RequestData = Dict[str, UserData]
+ComplexData = List[Optional[int]]
+ReplyData = Dict[str, Union[Tuple[str, str, bool], ComplexData]]
+
+
+def parse_user_info(json: RequestData) -> ReplyData:
+    user = json['user']
+    complexes = user['complexes']
+    return {'user': (user['login'], user['password'], user['is_admin']),
+            'complexes': complexes}
+
+
+def parse_admin_info(json) -> Tuple[str, str]:
+    admin = json['admin']
+    return admin['login'], admin['password']
