a98164bd3f60bfaf096393726ad381e21ed09ba1
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-19 20:11:58 +0300

    ВОТ ОНО ДОЛГОЖДАННОЕ ОБНОВЛЕНИЕ 27.07.21. СВЕРШИЛОСЬ ТО, ЧЕГО ВЫ ВСЕ ТАК ДОЛГО ЖДАЛИ!!!
    В ОБНОВЛЕНИИ БЫЛИ ДОБАВЛЕНЫ
    
    МАТРИЦА ИЗ СЛУЧАЙНЫХ ЦИФР
    ТОЛКОВЫЙ СЛОВАРЬ ДАЛЯ
    ИНСТРУКЦЯ ПО СБОРКЕ КИТАЙСКОГО ЛЕГО
    СТУДЕНЧЕСКИЙ БАТОН, ИЗГОТОВЛЕННЫЙ В СООТВЕТСТВИИ С ГОСТ 27844-88
    И МНОГОЕ ДРУГОЕ!

---
+++ server/request_examples/create-user.sh
@@ -0,0 +1,4 @@
+curl    --header "Content-Type: application/json" \
+        --request POST \
+        --data @create-user.json \
+        http://localhost:5000/create-user
---
+++ server/request_examples/get-telemetry.sh
@@ -0,0 +1,5 @@
+curl    --header "Content-Type: application/json" \
+        --request GET -sL \
+        --data @get-telemetry.json \
+        http://localhost:5000/get-data
+
