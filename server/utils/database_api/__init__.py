95cd55ea111227efa5c8f8e3872040730f1e11a4
Urmomus <yakov4enko.nikolay@yandex.ru>
2022-03-30 19:47:19 +0300

    minor changes

---
+++ server/utils/database_api/__init__.py
@@ -0,0 +1,4 @@
+from .database import Database
+from .models import User, Telemetry, Complex, Admin
+
+__all__ = ['Database', 'User', 'Telemetry', 'Complex', 'Admin']
--- server/utils/database_api/models/__init__.py
+++ server/utils/database_api/models/__init__.py
@@ -1,3 +1,5 @@
-from .user import User
+from .user import User, Admin
 from .complex import Complex
 from .telemetry import Telemetry
+
+__all__ = ['User', 'Complex', 'Telemetry', 'Admin']
